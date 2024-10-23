728661ba6d9adcee925e2e822f3f5b209d9d7b4c
Andrea Castro <ap.castrocastellon@gmail.com>
2024-10-23 04:27:56 -0300

    index y gitignore

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+
+app.use(express.json());
+
+
+// const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Railway y Vercel necesitan este ajuste para conexiones SSL
+  },
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`Base de datos conectada: ${result.rows[0].now}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al conectar a la base de datos');
+  }
+});
+
+// app.listen(port, () => {
+//  console.log(`Servidor ejecutándose en el puerto ${port}`);
+// });
+
+module.exports = app;
